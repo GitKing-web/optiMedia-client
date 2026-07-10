7f040ef5ffde688f3e18b3ca735bb65f53a6a091
KhalidMujahid <binkhalid267@gmail.com>
2026-07-10 22:27:00 +0100

    comment seed

--- server/index.ts
+++ server/index.ts
@@ -4,7 +4,7 @@ import { seedDatabase } from './src/data/seed.ts'
 const PORT = Number(process.env.PORT || 3000)
 
 try {
-  await seedDatabase()
+  // await seedDatabase()
 
   app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`)
